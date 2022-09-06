89234cb3cb7ca0930ed6ca6c4d43e5fcc6155877
Thamir Qadah <qadah.thamir@gmail.com>
2022-09-07 01:55:53 +0300

    intial code

---
+++ src/main/java/edu/uqu/cs/App.java
@@ -0,0 +1,13 @@
+package edu.uqu.cs;
+
+/**
+ * This a Maven based Java project.
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
